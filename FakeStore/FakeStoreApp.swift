a1abc0a153a600f32ce8de11e3b593678fa95dfb
MAZUREK, MAGORZATA <mz0502@exo.att.com>
2024-07-29 18:36:04 +0200

    Initial Commit

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Malgorzata Mazurek on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
